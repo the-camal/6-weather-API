ee42797f28a734fdb73a889546cdb893b5c9240c
paulcourts <hubertcourts@gmail.com>
2022-10-16 17:45:34 -0500

    getting everthing working on back end

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const app = {
         .getElementById('btnCurrent')
         .addEventListener('click', app.getLocation);
     },
+    // this is going to get the info from openweather app and my html
     fetchWeather: (ev) => {
     
       let city = document.getElementById('latitude').value;
@@ -24,7 +25,7 @@ const app = {
         })
         .catch(console.err);
     },
-
+// how long it is going to take
     getLocation: (ev) => {
         let opts = {
           enableHighAccuracy: true,
@@ -46,53 +47,12 @@ const app = {
       showWeather: (resp) => {
         console.log(resp);
         let town = document.querySelector('.weather.row');
-        document.querySelector('.temp') .innerText= "temp, "+resp.main.temp
-        document.querySelector('.city') .innerText= "city, "+resp.name
+        document.querySelector('.temp') .innerText= "temp = "+resp.main.temp
+        document.querySelector('.city') .innerText= "city = "+resp.name
         document.querySelector('.weathericon') .src= `http://openweathermap.org/img/wn/${resp.weather[0].icon}@4x.png`
-        document.querySelector('.wind') .innerText= 'windspeed, '+resp.wind.speed
-        document.querySelector('humidity') .innerText= 'humidity'+resp.humidity
-        
-        //  town.innerHTML = resp.daily
-      //    .map((day, idx) => {
-      //       if (idx <= 2) {
-      //         let dt = new Date(day.dt * 1000);
-      //         let sr = new Date(day.sunrise * 1000).toTimeString();
-      //         let ss = new Date(day.sunset * 1000).toTimeString();
-      //         return `<div class="col">
-      //             <div class="clip">
-      //             <h5 class="clip-title p-2">${dt.toDateString()}</h5>
-      //               <img
-      //                 src="http://openweathermap.org/img/wn/${
-      //                   day.weather[0].icon
-      //                 }@4x.png"
-      //                 class="clip-img-top"
-      //                 alt="${day.weather[0].description}"
-      //               />
-      //               <div class="clip-body">
-      //                 <h3 class="clip-title">${day.weather[0].main}</h3>
-      //                 <p class="clip-text">High ${day.temp.max}&deg;C Low ${
-      //           day.temp.min
-      //         }&deg;C</p>
-      //                 <p class="clip-text">High Feels like ${
-      //                   day.feels_like.day
-      //                 }&deg;C</p>
-      //                 <p class="clip-text">Pressure ${day.pressure}mb</p>
-      //                 <p class="clip-text">Humidity ${day.humidity}%</p>
-      //                 <p class="clip-text">UV Index ${day.uvi}</p>
-      //                 <p class="clip-text">Precipitation ${day.pop * 100}%</p>
-      //                 <p class="clip-text">Dewpoint ${day.dew_point}</p>
-      //                 <p class="clip-text">Wind ${day.wind_speed}m/s, ${
-      //           day.wind_deg
-      //         }&deg;</p>
-      //                 <p class="clip-text">Sunrise ${sr}</p>
-      //                 <p class="clip-text">Sunset ${ss}</p>
-      //               </div>
-      //             </div>
-      //           </div>
-      //         </div>`;
-      //       }
-      //     })
-      //     .join(' ');
+        document.querySelector('.wind') .innerText= 'windspeed = '+resp.wind.speed
+        document.querySelector('.humidity') .innerText= 'humidity = '+resp.main.humidity
+   
       },
     };
     
